2dd2937cae9f4c209bf1a2a0c541d9c0076c747c
RegisYamamoto <regisyamamoto@gmail.com>
2019-03-15 22:46:44 -0300

    aula34

---
+++ src/loianeGroner/aula34/TesteCalculadora.java
@@ -0,0 +1,13 @@
+package loianeGroner.aula34;
+
+import loianeGroner.aula34.MinhaCalculadora;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		
+		System.out.println(MinhaCalculadora.soma(1, 2));
+
+	}
+
+}
