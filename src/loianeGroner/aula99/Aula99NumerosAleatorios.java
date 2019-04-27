b06fec8a664a83ac3047c2f5087d51792d682f29
RegisYamamoto <regisyamamoto@gmail.com>
2019-04-27 19:29:28 -0300

    aula99

---
+++ src/loianeGroner/aula99/Aula99NumerosAleatorios.java
@@ -0,0 +1,20 @@
+package loianeGroner.aula99;
+
+import java.util.Random;
+
+// Números Aleatórios (Rancom)
+
+public class Aula99NumerosAleatorios {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.floor(Math.random() * 100));
+		
+		Random aleatorio = new Random();
+		
+		System.out.println(aleatorio.nextInt());
+		System.out.println(aleatorio.nextInt(5 + 1));
+
+	}
+
+}
