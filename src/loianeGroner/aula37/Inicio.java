9d733ab8bee8a31665757c52023ac666fd0f0018
RegisYamamoto <regisyamamoto@gmail.com>
2019-06-01 20:10:19 -0300

    aula 37

---
+++ src/loianeGroner/aula37/Inicio.java
@@ -0,0 +1,26 @@
+package loianeGroner.aula37;
+
+// Orienção a Objetos: Herança
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		
+//		Aluno aluno = new Aluno();
+//		
+//		Professor professor = new Professor();
+//		
+//		Pessoa pessoa = new Pessoa();
+//		
+//		aluno.setNome("Maria");
+//		
+//		professor.setTelefoneCelular("43 9 9186 2854");
+		
+		Pessoa aluno = new Aluno();
+		//aluno.get
+		
+		Pessoa professor = new Professor();
+		//professor.get
+	}
+
+}
