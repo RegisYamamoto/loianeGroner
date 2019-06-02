61f13dc301c7c77194a229366e958fe682e3cda7
RegisYamamoto <regisyamamoto@gmail.com>
2019-06-01 21:13:27 -0300

    aula41

---
+++ src/loianeGroner/aula41/Teste.java
@@ -0,0 +1,24 @@
+package loianeGroner.aula41;
+
+import loianeGroner.aula41.Aluno;
+import loianeGroner.aula41.Pessoa;
+import loianeGroner.aula41.Professor;
+
+// Orientação a Objetos: Herança: classes abstratas
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Pessoa aluno = new Aluno();
+		Pessoa professor = new Professor();
+		
+		aluno.setEndereco("Rua 2, num 2");
+		professor.setEndereco("Rua 3, num 3");
+		
+		aluno.imprimirEtiquetaEndereco();
+		professor.imprimirEtiquetaEndereco();
+
+	}
+
+}
